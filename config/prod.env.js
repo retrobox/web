8fb9e59ec358531d2efb35f7b2047d7dcb7dcce3
Le_Futuriste <spamfree@matthieubessat.fr>
2018-06-12 14:42:29 +0200

    Finalizing partner ship & cc

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,8 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"'
+  // TWITTER_URL: '"https://twitter.com/retrobox"',
+  // FACEBOOK_URL: '"https://facebook.com/retrobox"',
+  // DISCORD_URL: '"https://discordapp.com/invite/Y7UHQ2"',
+  // GITHUB_URL: '"https://github.com/retrobox"'
 }
