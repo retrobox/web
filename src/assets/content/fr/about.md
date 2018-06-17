5874d19acb80bdc262c90ca333e5f7c394e68b7a
Le_Futuriste <spamfree@matthieubessat.fr>
2018-06-17 22:24:57 +0200

    Adding team members (with array shuffle)

--- src/assets/content/fr/about.md
+++ src/assets/content/fr/about.md
@@ -7,4 +7,5 @@ Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim just
 Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. 
 
 
-Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, 
+Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
+ 
--- src/components/About.vue
+++ src/components/About.vue
@@ -7,7 +7,19 @@
         </div>
         <div class="container mx-auto">
             <div class="article pt-4">
-                <div class="content" v-html="content">
+                <div class="content">
+                    <div v-html="content">
+
+                    </div>
+                    <h2>{{$t('our-team-members')}}</h2>
+
+                    <div class="team-mosaic">
+                        <div class="team-item" v-for="member in members">
+                            <img :src="member.image" alt="avatar">
+                            <p class="team-item-title">{{member.name}}</p>
+                            <p class="team-item-description">{{member.description}}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -19,24 +31,36 @@
         name: 'About',
         data () {
             return {
-                content: ""
+                members: [],
+                content: ''
             }
         },
         watch: {
-          '$i18n.locale' () {
-              this.fetchData()
-          }
+            '$i18n.locale' () {
+                this.fetchData()
+            }
         },
         methods: {
-          fetchData: function() {
-              this.$store.commit('SET_TITLE', {context: this, key:'about'})
-              this.$store.commit('SET_LOCATION', {
-                  root: this.$t('home'),
-                  sub_root: this.$t('about')
-              })
-              const about = require('../assets/content/' + this.$i18n.locale + '/about.md')
-              this.content = about
-          }
+            fetchData: function () {
+                this.$store.commit('SET_TITLE', {context: this, key: 'about'})
+                this.$store.commit('SET_LOCATION', {
+                    root: this.$t('home'),
+                    sub_root: this.$t('about')
+                })
+                var team = require('../assets/content/' + this.$i18n.locale + '/team.json')
+                //shuffle the team
+                var i = team.length;
+                while (--i) {
+                    var j = Math.floor(Math.random() * (i + 1))
+                    var temp = team[i];
+                    team[i] = team[j];
+                    team[j] = temp;
+                }
+                this.members = team
+
+                const content = require('../assets/content/' + this.$i18n.locale + '/about.md')
+                this.content = content
+            }
         },
         created () {
             this.fetchData()
