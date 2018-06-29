cc80adb1800a66e72f3305413e8ea6d94e3c04b2
Matthieu Bessat <spamfree@matthieubessat.fr>
2018-06-29 13:49:13 +0200

    Remove useless env var & custom style ajustement

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,7 +6,5 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   DOCS_ENDPOINT: '"http://localhost:9950"',
   API_ENDPOINT: '"http://localhost:8000"',
-  STRIPE_PUBLIC: '"pk_test_vyGtkeybeUHvLZnXFbaHlXWC"',
-  SHOP_ITEM_CONSOLE_BONUS_16GB: '"3.66"',
-  SHOP_ITEM_CONSOLE_BONUS_32GB: '"9.86"'
+  STRIPE_PUBLIC: '"pk_test_vyGtkeybeUHvLZnXFbaHlXWC"'
 })
