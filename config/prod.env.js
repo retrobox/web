054c83cc13cf7df391644f4b218d4e5a647abf13
Valentin Derouet <thevalentin61@gmail.com>
2018-06-29 14:08:24 +0200

    add , in .env file
    idiot

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   DOCS_ENDPOINT: '"https://docs.retrobox.tech"',
-  API_ENDPOINT: '"https://api.retrobox.tech"'
+  API_ENDPOINT: '"https://api.retrobox.tech"',
   STRIPE_PUBLIC: '"pk_test_owe0KjkxRTyq5eJsPqrWYcLC"'
 }
