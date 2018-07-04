9fa15ef7d67d9b5e6a0007918e9e8103e38e5f7e
Valentin Derouet <thevalentin61@gmail.com>
2018-07-04 17:12:37 +0200

    WIP : Lang update

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -42,14 +42,32 @@
               <div>
                 <h2 class="dividing">Par email</h2>
                 <p>
-                  En envoyant un email à : <a href="mailto:contact@retrobox.tech">contact@retrobox.tech</a>
+                  En nous envoyant un email à: <a href="mailto:contact@retrobox.tech">contact@retrobox.tech</a>
+                </p>
+                <h2 class="dividing">Par Discord</h2>
+                <p>
+                  En rejoignant notre serveur discord par : <a href="https://discord.gg/TzNNQnP">ce lien</a>
+                </p>
+
+                <h2 class="dividing">Avec Laposte</h2>
+                <p>
+                  Thingmill<br />
+                  1 chemin de badoire<br />
+                  Heloup, 61250, France<br />
+                </p>
+                <h2 class="dividing">Par téléphone</h2>
+                <p>
+                  +33 769997549
                 </p>
               </div>
               <div class="map">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2649.1831527803884!2d0.02048481564818878!3d48.39542577924495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e27535bafa57bb%3A0x7cd97fc8ad2efa55!2sThingmill!5e0!3m2!1sfr!2sfr!4v1530277084988" frameborder="0" style="border:0" allowfullscreen align="right"></iframe>
               </div>
             </div>
+            </div>
+
           </div>
+
           </div>
         </div>
       </div>
