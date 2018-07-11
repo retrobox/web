ea34aaf087e8ca67b094ac673178ac755fe1e337
Matthieu Bessat <spamfree@matthieubessat.fr>
2018-07-11 21:47:32 +0200

    final fix ?

--- src/components/Newsletter.vue
+++ src/components/Newsletter.vue
@@ -46,6 +46,7 @@
         },
         methods: {
             submit: function () {
+                console.log('new request')
                 this.loading = true
                 if (this.email != undefined && this.email != "") {
                     axios.post(process.env.MAILCHIMPER_ENDPOINT + '/subscribe', {
@@ -56,10 +57,12 @@
                           title: this.$t('newsletter.success.title'),
                           description: this.$t('newsletter.success.description')
                         })
-                            this.loading = false
+                        this.loading = false
                     }).catch((error) => {
-                        if (error.response) {
-                            if (error.response.data.error.slug) {
+                        this.loading = false
+                        if (error.response != undefined && error.response.status == 400) {
+                            if (
+                             error.response.data.errors.title == "Member Exists") {
                               this.$store.commit('ADD_ALERT', {
                                 type: 'error',
                                 title:  this.$t('newsletter.error.title'),
@@ -79,7 +82,6 @@
                             description: this.$t('newsletter.error.api_description')
                           })
                         }
-                        this.loading = false
                     })
                 }else{
                   this.$store.commit('ADD_ALERT', {
@@ -87,7 +89,7 @@
                     title:  this.$t('newsletter.error.title'),
                     description: this.$t('newsletter.error.missing_description')
                   })
-                      this.loading = false
+                  this.loading = false
                 }
             }
         }
