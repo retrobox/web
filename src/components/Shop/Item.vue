e5e70d3e914370092ea1bf60f2faad2e10320c65
Etienne Chevrollier <etienne.chevrollier@live.fr>
2018-12-04 19:28:09 +0100

    Trying

--- src/components/Shop/Item.vue
+++ src/components/Shop/Item.vue
@@ -82,7 +82,32 @@
                                         </div>
                                         <div class="social-sharing">
                                           <span>Partager</span>
-
+                                          <div class="socials">
+                                              <ul>
+                                                  <li class="facebook">
+                                                      <a :href="$t('facebook').link" :title="$t('facebook').title">
+                                                          <icon name="brands/facebook" class="icon"></icon>
+                                                      </a>
+                                                  </li>
+                                                  <li class="twitter">
+                                                      <a :href="$t('twitter').link" :title="$t('twitter').title">
+                                                          <icon name="brands/twitter" class="icon"></icon>
+                                                      </a>
+                                                  </li>
+                                                  <li class="github">
+                                                      <a :href="$t('github').link" :title="$t('github').title">
+                                                          <icon name="brands/github" class="icon"></icon>
+                                                      </a>
+                                                  </li>
+                                                  <li class="discord">
+                                                      <a :href="$t('discord').link" :title="$t('discord').title">
+                                                          <icon name="brands/discord" class="icon"></icon>
+                                                      </a>
+                                                  </li>
+                                              </ul>
+                                          </div>
+                                        </div>
+                                      <div class="buy-advantages">
                                         <div class="buy-ways-container">
                                             <!-- <div class="hint">Nous suportons ces moyens de payements :</div> -->
                                             <div class="buy-ways-mosaic">
