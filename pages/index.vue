282563a26f83b02be811f184a361f1c5f964c0e1
Matthieu Bessat <spamfree@matthieubessat.fr>
2018-12-08 11:38:04 +0100

    refactor: remove Logo component by default refactor: refactor index page refactor: remove blank line at the end of default layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,9 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        retrobox.web
-      </h1>
-      <h2 class="subtitle">
-        The retrobox website made with nuxt.js
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <div>home page</div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  }
 }
 </script>
 
