db76bea42f7e0dde6c5d83059b82a1f835eed0c8
Matthieu Bessat <spamfree@matthieubessat.fr>
2018-12-08 11:20:39 +0100

    refactor: delete dot file

--- .postcssrc.js
+++
@@ -1,9 +0,0 @@
-// https://github.com/michael-ciniawsky/postcss-load-config
-var tailwindcss = require('tailwindcss');
-module.exports = {
-  "plugins": {
-    "postcss-import": {},
-    // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
-  }
-}
