22af0cc036e08f7e8c8b56d3a7b86ded2be85de9
Le_Futuriste <spamfree@matthieubessat.fr>
2018-12-17 18:00:59 +0100

    feat: add static global favicon/icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,13 +20,28 @@ module.exports = {
       { hid: 'description', name: 'description', content: "Retrobox" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/png', href: '/favicon.png'},
+      {rel: 'apple-touch-icon', sizes: '57x57', href: '/apple-icon-57x57.png'},
+      {rel: 'apple-touch-icon', sizes: '60x60', href: '/apple-icon-60x60.png'},
+      {rel: 'apple-touch-icon', sizes: '72x72', href: '/apple-icon-72x72.png'},
+      {rel: 'apple-touch-icon', sizes: '76x76', href: '/apple-icon-76x76.png'},
+      {rel: 'apple-touch-icon', sizes: '114x144', href: '/apple-icon-144x144.png'},
+      {rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-icon-120x120.png'},
+      {rel: 'apple-touch-icon', sizes: '144x144', href: '/apple-icon-144x144.png'},
+      {rel: 'apple-touch-icon', sizes: '152x152', href: '/apple-icon-152x152.png'},
+      {rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-icon-180x180.png'},
+      {rel: 'icon', type: 'image/png', sizes: '16x16', href: '/android-icon-16x16.png'},
+      {rel: 'icon', type: 'image/png', sizes: '32x32', href: '/android-icon-36x36.png'},
+      {rel: 'icon', type: 'image/png', sizes: '96x96', href: '/android-icon-96x96.png'},
+      {rel: 'icon', type: 'image/png', sizes: '192x192', href: '/android-icon-192x192.png'},
+      {rel: 'manifest', href: '/manifest.json'}
     ]
   },
 
   router: {
     middleware: 'i18n',
     scrollBehavior: function () {
+      console.log('scroll behaviour debug')
       return { x: 0, y: 0 }
     }
   },
