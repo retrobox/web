9ae01e2cfe380e592b70ba5f863fad515ee0813e
Etienne Chevrollier <etienne.chevrollier@live.fr>
2019-04-02 21:50:47 +0200

    Download page add icons

--- pages/download.vue
+++ pages/download.vue
@@ -11,6 +11,24 @@
             {{ $t('download.page.description') }}
           </div>
 
+          <div class="os-icon">
+            <div class="win-icon os-icon"><i class="fab fa-windows"></i></div>
+            <div class="mac-icon os-icon"><i class="fab fa-apple"></i></div>
+            <div class="lin-icon os-icon"><i class="fab fa-linux"></i></div>
+          </div>
+
+          <div class="os-text">
+            <div class="win-text"><h2>Windows 7/8/10</h2></div>
+            <div class="mac-text"><h2>Mac OS</h2></div>
+            <div class="lin-text"><h2>Linux</h2></div>
+          </div>
+
+          <div class="os-icon">
+            <div class="android-icon os-icon"><i class="fab fa-android"></i></div>
+            <div class="ios-icon os-icon"><i class="fab fa-apple"></i></div>
+          </div>
+
+          <div class="coming-soon"><h2>Bientôt sur Android et iOS</h2></div>
 
         </div>
       </div>
