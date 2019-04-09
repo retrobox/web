02d4fd8e3caab3bb9c4923856f2124d9be9e7711
Le_Futuriste <spamfree@matthieubessat.fr>
2019-04-09 15:36:26 +0200

    fix(download): fix download style

--- pages/downloads.vue
+++ pages/downloads.vue
@@ -1,36 +1,39 @@
 <template>
   <div class="downloads">
-    <div class="downloads-content">
+    <div class="downloads-content container mx-auto">
       <div class="downloads-title-container">
         <div>
           <h2 class="downloads-title">
             {{ $t('downloads.page.title') }}
           </h2>
-
-          <div class="downloads-description">
-            {{ $t('downloads.page.description') }}
-          </div>
-
-          <div class="os-icon">
-            <div class="win-icon os-icon"><i class="fab fa-windows"></i></div>
-            <div class="mac-icon os-icon"><i class="fab fa-apple"></i></div>
-            <div class="lin-icon os-icon"><i class="fab fa-linux"></i></div>
-          </div>
-
-          <div class="os-text">
-            <div class="win-text"><h2>{{ $t('downloads.page.os-text.win') }}</h2></div>
-            <div class="mac-text"><h2>{{ $t('downloads.page.os-text.mac') }}</h2></div>
-            <div class="lin-text"><h2>{{ $t('downloads.page.os-text.lin') }}</h2></div>
-          </div>
-
-          <div class="os-icon">
-            <div class="android-icon os-icon"><i class="fab fa-android"></i></div>
-            <div class="ios-icon os-icon"><i class="fab fa-apple"></i></div>
-          </div>
-
-          <div class="coming-soon"><h2>{{ $t('downloads.page.os-text.coming-soon') }}</h2></div>
-
         </div>
+      </div>
+      <div class="downloads-container">
+        <div class="downloads-description">
+          {{ $t('downloads.page.description') }}
+        </div>
+
+        <div class="os-desktop">
+          <div class="os-desktop-item win">
+            <i class="icon fab fa-windows"></i>
+            <div class="text">{{ $t('downloads.page.os-text.win') }}</div>
+          </div>
+          <div class="os-desktop-item mac">
+            <i class="icon fab fa-apple"></i>
+            <div class="text">{{ $t('downloads.page.os-text.mac') }}</div>
+          </div>
+          <div class="os-desktop-item lin">
+            <i class="icon fab fa-linux"></i>
+            <div class="text">{{ $t('downloads.page.os-text.lin') }}</div>
+          </div>
+        </div>
+
+        <div class="os-mobile">
+          <div class="os-mobile-item android"><i class="fab fa-android icon"></i></div>
+          <div class="os-mobile-item apple"><i class="fab fa-apple icon"></i></div>
+        </div>
+
+        <div class="coming-soon"><h2>{{ $t('downloads.page.os-text.coming-soon') }}</h2></div>
       </div>
     </div>
   </div>
