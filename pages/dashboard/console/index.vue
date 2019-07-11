adbfc3727c3908f5d89eec5fa3eddbf89d6cb67e
Le_Futuriste <spamfree@matthieubessat.fr>
2019-07-11 16:30:46 +0200

    feat(Dashboard): base of consoles list

--- pages/dashboard/console/index.vue
+++ pages/dashboard/console/index.vue
@@ -3,9 +3,19 @@
     <div class="dashboard-page">
       <DashboardNav />
       <div class="dashboard-page-content">
-        <h1 class="dashboard-page-title">
+        <!-- <h1 class="dashboard-page-title">
           WIP [consoles]
-        </h1>
+        </h1> -->
+        <div 
+          v-if="consoles.length !== 0" 
+          class="shop-empty-container no-top-margin">
+          <div class="shop-empty-icon">
+            <Icon value="fas fa-times-circle" />
+          </div>
+          <div class="shop-empty-title">
+            <h3>Vous n'avez pas encore de consoles</h3>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -13,15 +23,30 @@
 
 <script>
 import DashboardNav from "../../../components/DashboardNav"
+import Icon from "../../../components/Icon"
+import Error from "../../../components/Error"
 
 export default {
   middleware: 'authenticated',
   components: {
-    DashboardNav
+    DashboardNav,
+    Icon,
+    Error
   },
   head() {
     return {
       title: "List of consoles"
+    }
+  },
+  data: () => ({
+    consoles: []
+  }),
+  async asyncData({ app: { apitator } }) {
+    let res = await apitator.graphQL(`query{getManyConsoles{
+      id
+    }}`, {}, {withAuth: true})
+    return {
+      consoles: res.data.data.getManyConsoles
     }
   }
 }
