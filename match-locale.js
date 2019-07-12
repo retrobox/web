6bd8ede35c0d59c4d5c68be87b32f686afbfb7c5
Le_Futuriste <spamfree@matthieubessat.fr>
2019-07-12 10:28:58 +0200

    fix(i18n): fix match locale cli tool

--- match-locale.js
+++ match-locale.js
@@ -7,7 +7,7 @@ let locales = [
 let keys = [];
 const all_keys = require('all-object-keys');
 locales.forEach((locale) => {
-  keys[locale._id] = all_keys(locale)
+  keys[locale.id] = all_keys(locale)
 });
 const colors = require('colors');
 const colors_a = [
@@ -19,13 +19,14 @@ const colors_a = [
   'cyan'
 ];
 let errors = 0;
-locales.forEach((origin_locale) => {
+
+locales.forEach((originLocale) => {
   locales.forEach((locale) => {
     let color = colors_a[Math.floor(Math.random()*colors_a.length)];
     colors_a.splice(colors_a.indexOf(color), 1);
-    keys[origin_locale._id].forEach((item) => {
-      if (keys[locale._id].indexOf(item) === -1) {
-        console.log(colors[color](item + " in ") + colors.bold(locale._id));
+    keys[originLocale.id].forEach((item) => {
+      if (keys[locale.id].indexOf(item) === -1) {
+        console.log(colors[color](item + " in ") + colors.bold(locale.id));
         errors++
       }
     })
