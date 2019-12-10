60a229b901996a4a163d139d65ba4d3431cfc806
Matthieu Bessat <spamfree@matthieubessat.fr>
2019-12-10 20:13:01 +0100

    fix(Shop):: little responsive css fix in shop item page

--- pages/shop/_slug.vue
+++ pages/shop/_slug.vue
@@ -80,7 +80,7 @@
                     v-if="item.show_version"
                     class="buy-custom-type">
                     <div class="buy-custom-title">{{ $t('shop.item.custom.model') }}</div>
-                    <div class="buy-custom-mosaic model">
+                    <div class="buy-custom-mosaic buy-custom-version-mosaic model">
                       <!-- <li class="-mb-px mr-1">
                         <a class="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-dark font-semibold" href="#">Version kité</a>
                       </li> -->
