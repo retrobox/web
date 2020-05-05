1d7b3631ace9bf2092441d833e5d23a3396bef04
Matthieu Bessat <spamfree@matthieubessat.fr>
2020-05-06 00:03:23 +0200

    fix: yes it was a dumb move

--- plugins/imageAdaptor.js
+++ plugins/imageAdaptor.js
@@ -5,8 +5,7 @@ export default function (context) {
   Vue.use({
     install: (context) => {
       context.prototype.$imageAdapter = (url, options = {}) => {
-        let isSafari = /^((?!chrome|android).)*safari/i.test(userAgent);
-        isSafari = true
+        let isSafari = /^((?!chrome|android).)*safari/i.test(userAgent)
         options = { fallback: '.png', ...options }
         let result = url + (isSafari ? options.fallback : '.webp')
         if (options.css) {
